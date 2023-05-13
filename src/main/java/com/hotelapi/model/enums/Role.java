82761b738c24af6a879294d54ba62fbb39c97370
Mario Guadalupe <mariogr93@gmail.com>
2023-05-13 19:04:56 -0400

    add role and permission

---
+++ src/main/java/com/hotelapi/model/enums/Role.java
@@ -0,0 +1,46 @@
+package com.hotelapi.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.hotelapi.model.enums.Permission.*;
+
+
+@RequiredArgsConstructor
+public enum Role {
+
+  USER(
+          Set.of(
+                  USER_CREATE,
+                  USER_READ,
+                  USER_UPDATE,
+                  USER_DELETE
+          )
+  ),
+  ADMIN(
+          Set.of(
+                  ADMIN_CREATE,
+                  ADMIN_READ,
+                  ADMIN_UPDATE,
+                  ADMIN_DELETE
+          )
+  );
+
+  @Getter
+  private final Set<Permission> permissions;
+
+  public List<SimpleGrantedAuthority> getAuthorities() {
+    var authorities = getPermissions()
+            .stream()
+            .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+            .collect(Collectors.toList());
+    authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    return authorities;
+  }
+
+}
