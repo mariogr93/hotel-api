8a1913cc261616a0f6e20baf1a701ec5bc46ba4f
Mario Guadalupe <mariogr93@gmail.com>
2023-05-26 22:18:22 -0400

    basic booking repository and service

---
+++ src/main/java/com/hotelapi/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.hotelapi.repository;
+
+import com.hotelapi.model.entity.BookingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<BookingEntity, Integer> {
+}
---
+++ src/main/java/com/hotelapi/service/BookingService.java
@@ -0,0 +1,26 @@
+package com.hotelapi.service;
+
+import com.hotelapi.model.entity.BookingEntity;
+import com.hotelapi.model.request.BookingDTO;
+import com.hotelapi.repository.BookingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookingService {
+
+
+    private final BookingRepository bookingRepository;
+
+    public BookingEntity createBooking(BookingDTO bookingDTO) {
+        BookingEntity booking = bookingDTO.createBookingEntity();
+        return this.bookingRepository.save(booking);
+    }
+    public List<BookingEntity> getAllBookings() {
+
+        return this.bookingRepository.findAll();
+    }
+}
