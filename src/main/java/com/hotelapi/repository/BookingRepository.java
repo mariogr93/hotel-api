903ee54c46e67d9d4f170653a1f77eb8be04a6b4
Mario Guadalupe <mariogr93@gmail.com>
2023-05-27 22:05:18 -0400

    create query for new findAllByClientId method

--- src/main/java/com/hotelapi/repository/BookingRepository.java
+++ src/main/java/com/hotelapi/repository/BookingRepository.java
@@ -2,8 +2,17 @@ package com.hotelapi.repository;
 
 import com.hotelapi.model.entity.BookingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, Integer> {
+
+    @Query("""
+    Select n from BookingEntity n inner join ClientEntity u on n.clientEntity.id = u.id where u.id = :clientId
+    """)
+    ArrayList<BookingEntity> findAllByClientId(Integer clientId);
 }
