9cf1895141eff2e36d749d26930b9d173da0a067
Mario Guadalupe <mariogr93@gmail.com>
2023-05-28 17:25:48 -0400

    name fix

--- src/main/java/com/hotelapi/model/request/BookingDTO.java
+++ src/main/java/com/hotelapi/model/request/BookingDTO.java
@@ -23,31 +23,13 @@ public class BookingDTO {
     private Integer numberOfAdults;
     @NotNull(message = "The  number of children is required.")
     private Integer numberOfChildren;
-    @NotNull(message = "The room is required.")
+    @NotNull(message = "The client is required.")
     private ClientEntity client;
     @NotNull(message = "The room is required.")
     private RoomEntity roomEntity;
-//    @NotNull(message = "The  hotel is required.")
-//    private HotelEntity hotel;
+
 
     public BookingEntity createBookingEntity(){
         return new BookingEntity(date, arraivalDate, departureDate, numberOfAdults, numberOfChildren, client, roomEntity, roomEntity.getHotel());
     }
 }
-
-
-//{
-//        "date":"01-01-2024",
-//        "arraivalDate":"01-01-2024",
-//        "departureDate":"01-01-2024",
-//        "numberOfAdults":2,
-//        "numberOfChildren":0,
-//        "roomEntity":{"id":1, "roomNumber":11, "hotelRoomCode":"a-single-1", "occupancy":false, "hotel":{"id":1, "hotelName":"hotel 5 stars", "address":"address", "city":"samana city", "country":"Dom Rep"}, "roomType": {"id":1, "roomType": "SINGLE_BED", "roomPrice": 30, "roomCapacity": 2}},
-//        "hotel":{
-//        "id":1,
-//        "hotelName":"hotel 5 stars",
-//        "address":"address",
-//        "city":"samana city",
-//        "country":"Dom Rep"
-//        }
-//        }
