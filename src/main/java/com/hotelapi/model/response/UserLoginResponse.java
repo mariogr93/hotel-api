a7c7d57c42b7d212150896a932233f8bbdd3b93f
Mario Guadalupe <mariogr93@gmail.com>
2023-05-27 22:01:08 -0400

    modify general user register and login response

--- src/main/java/com/hotelapi/model/response/UserLoginResponse.java
+++ src/main/java/com/hotelapi/model/response/UserLoginResponse.java
@@ -1,22 +1,39 @@
 package com.hotelapi.model.response;
 
+import com.hotelapi.model.enums.Gender;
 import com.hotelapi.model.enums.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
-@SuperBuilder
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserLoginResponse {
 
     private Integer id;
     private String firstName;
     private String lastName;
-    private String token;
+    private Integer phoneNumber;
     private String email;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
     private Role role;
+    private String token;
 
+    public UserLoginResponse() {
+    }
+
+    public UserLoginResponse(Integer id, String firstName, String lastName, Integer phoneNumber, String email, Gender gender, Role role, String token) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.gender = gender;
+        this.role = role;
+        this.token = token;
+    }
 }
--- src/main/java/com/hotelapi/model/response/UserRegisterResponse.java
+++ src/main/java/com/hotelapi/model/response/UserRegisterResponse.java
@@ -1,13 +1,35 @@
 package com.hotelapi.model.response;
 
 
-import com.hotelapi.config.security.UserDetailsImpl;
-import lombok.Data;
+import com.hotelapi.model.enums.Gender;
+import com.hotelapi.model.enums.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
 
 @Data
 public class UserRegisterResponse {
 
-    private String message;
-    private UserDetailsImpl user;
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer phoneNumber;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public UserRegisterResponse() { }
+
+    public UserRegisterResponse(Integer id, String firstName, String lastName, Integer phoneNumber, String email, Gender gender, Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.gender = gender;
+        this.role = role;
+    }
 
 }
