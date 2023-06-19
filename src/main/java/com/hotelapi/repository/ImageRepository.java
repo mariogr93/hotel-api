455c921e47f94528462a3f4d3b53202eb1c4f2a5
Mario Guadalupe <mariogr93@gmail.com>
2023-06-18 21:52:28 -0400

    create findAllByRoomId query and method

--- src/main/java/com/hotelapi/repository/ImageRepository.java
+++ src/main/java/com/hotelapi/repository/ImageRepository.java
@@ -5,10 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
+    @Query("""
+    Select i from ImageEntity i inner join RoomEntity r on i.roomEntity.id = r.id where r.id = :roomId
+    """)
+    List<ImageEntity> findAllByRoomId(Integer roomId);
+
     @Query("""
     Select n from ImageEntity n where n.name = :fileName
     """)
